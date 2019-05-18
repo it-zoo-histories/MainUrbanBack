ced00ed645109f299820a849d92381b0907684fd
kostya05983 <kostya05983@mail.ru>
2019-05-19 01:47:00 +0700

    [ADD] fix errors with zuul

--- zuul-server/src/main/java/com/stopysinger/core/zuul/model/User.java
+++ zuul-server/src/main/java/com/stopysinger/core/zuul/model/User.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "users", uniqueConstraints = {
+@Table(name = "keks", uniqueConstraints = {
         @UniqueConstraint(columnNames = "email")
 })
 public class User {
@@ -20,8 +20,6 @@ public class User {
     @Email
     @Column(nullable = false)
     private String email;
-
-    private String imageUrl;
 
     @Column(nullable = false)
     private Boolean emailVerified = false;
@@ -57,14 +55,6 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
     }
 
     public Boolean getEmailVerified() {
