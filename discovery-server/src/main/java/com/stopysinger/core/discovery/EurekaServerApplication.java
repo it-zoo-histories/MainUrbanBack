4f11a203182a984ed66af7bca8c6c213b74653b2
Pavel Istomin <palevrut@gmail.com>
2019-05-18 16:25:37 +0700

    stopysinger-back: discovery server

---
+++ discovery-server/src/main/java/com/stopysinger/core/discovery/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.stopysinger.core.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
